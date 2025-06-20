583bfdf0ed9ab11a65f4c9a035f16a06ee0648c5
qhfka0090 <white1322@hanmail.net>
2025-06-20 17:13:26 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,9 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/cinema'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
 )
+
